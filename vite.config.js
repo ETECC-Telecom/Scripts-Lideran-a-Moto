7d70eb3f44f1f465c59d0670139bc254f3a92bec
RaphaelAnjos27 <raphael.anjos@outlook.com.br>
2026-02-18 14:01:24 -0300

    Ajustes nos caminhos

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // O ponto antes da barra faz os caminhos ficarem relativos (./)
   // Isso corrige o erro de MIME type e 404 no mobile/GitHub Pages
-  base: './Scripts-Lideran-a-Moto/', 
+  base: './', 
   build: {
     outDir: 'docs', // Faz o build ir para a pasta docs automaticamente
   },
