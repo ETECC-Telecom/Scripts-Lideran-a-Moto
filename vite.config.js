8b1933f01a8f80a4c8e877e2d3eadef99d31ccdd
RaphaelAnjos27 <raphael.anjos@outlook.com.br>
2026-02-18 13:37:14 -0300

    Primeira Build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // O ponto antes da barra faz os caminhos ficarem relativos (./)
   // Isso corrige o erro de MIME type e 404 no mobile/GitHub Pages
-  base: './', 
+  base: './Scripts-Lideran-a-Moto/', 
   build: {
     outDir: 'docs', // Faz o build ir para a pasta docs automaticamente
   },
